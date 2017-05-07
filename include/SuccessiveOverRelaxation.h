a63d67112578a759e05edf630e333074273bc8d1
Diego Assencio <diego@assencio.com>
2017-05-07 21:53:05 +0200

    updates successive over-relaxation solver for linear systems

---
+++ include/SuccessiveOverRelaxation.h
@@ -0,0 +1,119 @@
+#pragma once
+
+#include <IterativeLinearSystemSolverBase.h>
+
+namespace freeaml
+{
+template<typename T>
+class SuccessiveOverRelaxation : public IterativeLinearSystemSolverBase<T>
+{
+public:
+    using BaseSolver = IterativeLinearSystemSolverBase<T>;
+    using size_type = typename BaseSolver::size_type;
+
+    /**
+     * @brief Constructs a linear system solver which uses the successive
+     *        over-relaxation method (SOR).
+     * @param max_iterations The maximum number of iterations allowed.
+     * @param residual_tolerance The maximum residual tolerance allowed.
+     * @param omega The relaxation factor.
+     */
+    SuccessiveOverRelaxation(size_type max_iterations,
+                             const T& residual_tolerance,
+                             const T& omega);
+
+    /**
+     * @brief Solves a linear system using the successive over-relaxation method
+     *        (SOR).
+     * @param A The linear system matrix.
+     * @param x The vector on which the solution will be written.
+     * @param b The right-hand side of the linear system.
+     * @return @c true if the residual tolerance could be achieved within the
+     *         maximum number of iterations allowed, @c false otherwise.
+     */
+    template<typename MatrixType, typename VectorType>
+    bool solve(const MatrixType& A, VectorType& x, const VectorType& b);
+
+    /**
+     * @brief Gets the relaxation factor set for the solver.
+     * @return The relaxation factor set for the solver.
+     */
+    const T& omega() const;
+
+private:
+    const T omega_;
+
+}; /* SuccessiveOverRelaxation<T> */
+
+/*******************************************************************************
+ *
+ *    FUNCTION DEFINITIONS
+ *
+ ******************************************************************************/
+
+template<typename T>
+SuccessiveOverRelaxation<T>::SuccessiveOverRelaxation(
+    const size_type max_iterations, const T& residual_tolerance, const T& omega)
+    : BaseSolver(max_iterations, residual_tolerance), omega_(omega)
+{
+    FREEAML_ASSERT(omega > T{0});
+}
+
+template<typename T>
+template<typename MatrixType, typename VectorType>
+bool SuccessiveOverRelaxation<T>::solve(const MatrixType& A,
+                                        VectorType& x,
+                                        const VectorType& b)
+{
+    FREEAML_ASSERT(BaseSolver::check_dimensions(A, x, b));
+
+    VectorType x0 = x;
+
+    (*this).num_iterations_ = 0;
+
+    while ((*this).num_iterations_ < (*this).max_iterations())
+    {
+        for (size_type i = 0; i < x.size(); ++i)
+        {
+            /* if A(i,i) = 0, the SOR method cannot be used */
+            if (A(i, i) == T{0})
+            {
+                return false;
+            }
+
+            T c = b[i];
+
+            for (size_type j = 0; j < i; ++j)
+            {
+                c -= A(i, j) * x[j];
+            }
+
+            for (size_type j = i + 1; j < A.num_rows(); ++j)
+            {
+                c -= A(i, j) * x0[j];
+            }
+
+            x[i] = (T{1} - omega()) * x0[i] + omega() * (c / A(i, i));
+        }
+
+        ++(*this).num_iterations_;
+
+        /* if the residual is within the maximum tolerance, stop */
+        if ((b - A * x).l2_norm() <= (*this).residual_tolerance())
+        {
+            return true;
+        }
+
+        x0 = x;
+    }
+
+    return false;
+}
+
+template<typename T>
+const T& SuccessiveOverRelaxation<T>::omega() const
+{
+    return omega_;
+}
+
+} /* namespace aml */
---
+++ tests/SuccessiveOverRelaxation.cpp
@@ -0,0 +1,79 @@
+#include <Matrix.h>
+#include <SuccessiveOverRelaxation.h>
+#include <Vector.h>
+#include <gtest/gtest.h>
+
+TEST(SuccessiveOverRelaxationTest, Solve4x4LinearSystem)
+{
+    /* linear system matrix */
+    freeaml::Matrix<double> A = {{10.0, -1.0, 2.0, 0.0},
+                                 {-1.0, 11.0, -1.0, 3.0},
+                                 {2.0, -1.0, 10.0, -1.0},
+                                 {0.0, 3.0, -1.0, 8.0}};
+
+    /* solution vector (initially set to zero) */
+    freeaml::Vector<double> x = {0.0, 0.0, 0.0, 0.0};
+
+    /* right-hand side */
+    freeaml::Vector<double> b = {6.0, 25.0, -11.0, 15.0};
+
+    /*
+     * Linear system solver: successive over-relaxation
+     * Maximum number of iterations: 50
+     * Residual tolerance required: 1e-7
+     * Relaxation factor: 1.5
+     */
+    freeaml::SuccessiveOverRelaxation<double> lss(50, 1e-7, 1.5);
+
+    /* solve the linear system */
+    bool status = lss.solve(A, x, b);
+
+    EXPECT_TRUE(status);
+
+    double residual = (A * x - b).l2_norm();
+
+    EXPECT_LE(residual, lss.residual_tolerance());
+    EXPECT_LE(lss.num_iterations(), lss.max_iterations());
+
+    std::cout << "Solution obtained: " << x << "\n"
+              << "Residual achieved: " << residual << "\n"
+              << "Iterations performed: " << lss.num_iterations() << "\n";
+}
+
+TEST(SuccessiveOverRelaxationTest, Solve5x5LinearSystem)
+{
+    /* linear system matrix */
+    freeaml::Matrix<double> A = {{5.85, 1.05, 1.80, 0.78, 1.16},
+                                 {1.05, 6.52, 0.57, 1.09, 0.34},
+                                 {1.80, 0.57, 7.47, 1.01, 1.31},
+                                 {0.78, 1.09, 1.01, 4.07, 0.15},
+                                 {1.16, 0.34, 1.31, 0.15, 4.37}};
+
+    /* solution vector (initially set to zero) */
+    freeaml::Vector<double> x = {0.0, 0.0, 0.0, 0.0, 0.0};
+
+    /* right-hand side */
+    freeaml::Vector<double> b = {13.13, 18.15, 3.74, 14.76, 18.46};
+
+    /*
+     * Linear system solver: successive over-relaxation
+     * Maximum number of iterations: 50
+     * Residual tolerance required: 1e-8
+     * Relaxation factor: 1.25
+     */
+    freeaml::SuccessiveOverRelaxation<double> lss(50, 1e-8, 1.25);
+
+    /* solve the linear system */
+    bool status = lss.solve(A, x, b);
+
+    EXPECT_TRUE(status);
+
+    double residual = (A * x - b).l2_norm();
+
+    EXPECT_LE(residual, lss.residual_tolerance());
+    EXPECT_LE(lss.num_iterations(), lss.max_iterations());
+
+    std::cout << "Solution obtained: " << x << "\n"
+              << "Residual achieved: " << residual << "\n"
+              << "Iterations performed: " << lss.num_iterations() << "\n";
+}
