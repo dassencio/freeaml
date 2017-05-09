345967995d42dafb5f85cb7ddcb86aab62840aea
Diego Assencio <diego@assencio.com>
2017-05-09 10:30:38 +0200

    updates Gaussian elimination solver for linear systems (merged gs_bs and ge_pp into one)

---
+++ include/GaussianElimination.h
@@ -0,0 +1,162 @@
+#pragma once
+
+#include <IterativeLinearSystemSolverBase.h>
+#include <Vector.h>
+#include <cmath>
+
+namespace freeaml
+{
+template<typename T>
+class GaussianElimination
+{
+public:
+    using size_type = size_t;
+
+    /**
+     * @brief Constructs a linear system solver which uses the Gaussian
+     *        elimination method.
+     * @param enable_partial_pivoting @c true if partial pivoting should be
+     *        used, @c false otherwise.
+     */
+    GaussianElimination(bool enable_partial_pivoting = true);
+
+    /**
+     * @brief Solves a linear system using the Gaussian elimination method.
+     * @param A The linear system matrix.
+     * @param x The vector on which the solution will be written.
+     * @param b The right-hand side of the linear system.
+     * @return @c true if the linear system has exactly one solution (i.e., if
+     *         @c A is invertible), @c false otherwise.
+     */
+    template<typename MatrixType, typename VectorType>
+    bool solve(MatrixType A, VectorType& x, VectorType b);
+
+    /**
+     * @brief Gets the state set for the partial pivoting technique.
+     * @return @c true if partial pivoting is enabled, @c false otherwise.
+     */
+    bool is_partial_pivoting_enabled() const;
+
+private:
+    const bool enable_partial_pivoting_; /* true: enable partial pivoting */
+
+}; /* GaussianElimination<T> */
+
+/*******************************************************************************
+ *
+ *    FUNCTION DEFINITIONS
+ *
+ ******************************************************************************/
+
+template<typename T>
+GaussianElimination<T>::GaussianElimination(
+    const bool enable_partial_pivoting /* = true */)
+    : enable_partial_pivoting_(enable_partial_pivoting)
+{
+    /* nothing needs to be done here */
+}
+
+template<typename T>
+template<typename MatrixType, typename VectorType>
+bool GaussianElimination<T>::solve(MatrixType A, VectorType& x, VectorType b)
+{
+    FREEAML_ASSERT(
+        IterativeLinearSystemSolverBase<T>::check_dimensions(A, x, b) == true);
+
+    size_type n = A.num_rows();
+
+    freeaml::Vector<size_type> row(n, 0);
+
+    /* row index vector: used for "swapping" rows of A */
+    for (size_type i = 0; i < n; ++i)
+    {
+        row[i] = i;
+    }
+
+    /* for each row i of the matrix A */
+    for (size_type i = 0; i < n; ++i)
+    {
+        size_type p = i;
+
+        /*
+         * if partial pivoting is enabled:
+         *
+         *      find p such that |A(p,i)| = max_q|A(row[q],i)| for i <= q < n
+         *
+         * if partial pivoting is disabled:
+         *
+         *      find the first p such that A(row[p],i) != 0 for i <= p < n
+         */
+        if (enable_partial_pivoting_ == true)
+        {
+            for (size_type q = i + 1; q < n; ++q)
+            {
+                if (std::abs(A(row[p], i)) < std::abs(A(row[q], i)))
+                {
+                    p = q;
+                }
+            }
+
+            /* if A_(row[p],i) = 0, A is not invertible */
+            if (A(row[p], i) == T{0})
+            {
+                return false;
+            }
+        }
+        else
+        {
+            while (p < n && A(row[p], i) == T{0})
+            {
+                ++p;
+            }
+
+            /* if p reaches n, A is not invertible */
+            if (p == n)
+            {
+                return false;
+            }
+        }
+
+        /* "swap" rows i and p of A */
+        std::swap(row[p], row[i]);
+
+        for (size_type j = i + 1; j < n; ++j)
+        {
+            T m = A(row[j], i) / A(row[i], i);
+
+            for (size_type l = i + 1; l < n; ++l)
+            {
+                A(row[j], l) -= m * A(row[i], l);
+            }
+
+            b[row[j]] -= m * b[row[i]];
+        }
+    }
+
+    size_type i = n;
+
+    /* backward substitution */
+    while (i > 0)
+    {
+        --i;
+
+        T y = T{0};
+
+        for (size_type j = i + 1; j < n; ++j)
+        {
+            y += A(row[i], j) * x[j];
+        }
+
+        x[i] = (b[row[i]] - y) / A(row[i], i);
+    }
+
+    return true;
+}
+
+template<typename T>
+bool GaussianElimination<T>::is_partial_pivoting_enabled() const
+{
+    return enable_partial_pivoting_;
+}
+
+} /* namespace freeaml */
---
+++ tests/GaussianElimination.cpp
@@ -0,0 +1,61 @@
+#include <GaussianElimination.h>
+#include <Matrix.h>
+#include <Vector.h>
+#include <gtest/gtest.h>
+
+TEST(GaussianEliminationTest, Solve4x4LinearSystem)
+{
+    /* linear system matrix */
+    freeaml::Matrix<double> A = {{3.11, 4.20, 4.25, 1.53},
+                                 {1.59, 0.31, 3.00, 0.30},
+                                 {4.63, 0.25, 1.51, 4.38},
+                                 {3.70, 2.47, 2.94, 3.84}};
+
+    /* solution vector (initially set to zero) */
+    freeaml::Vector<double> x = {0.0, 0.0, 0.0, 0.0};
+
+    /* right-hand side */
+    freeaml::Vector<double> b = {8.79, -0.49, 8.00, 9.54};
+
+    /* linear system solver: Gaussian elimination without partial pivoting */
+    freeaml::GaussianElimination<double> lss(false);
+
+    /* solve the linear system */
+    bool status = lss.solve(A, x, b);
+
+    EXPECT_TRUE(status);
+
+    /* exact solution */
+    freeaml::Vector<double> y = {1.0, 2.0, -1.0, 1.0};
+
+    EXPECT_LE((x - y).l2_norm(), 1.e-10);
+}
+
+TEST(GaussianEliminationTest, Solve5x5LinearSystem)
+{
+    /* linear system matrix */
+    freeaml::Matrix<double> A = {{2.78, 1.57, 2.68, 4.38, 3.65},
+                                 {2.56, 1.94, 1.98, 1.75, 0.83},
+                                 {1.80, 1.25, 2.38, 0.62, 1.31},
+                                 {2.59, 2.13, 0.65, 1.67, 0.37},
+                                 {1.48, 2.52, 2.34, 0.53, 4.59}};
+
+    /* solution vector (initially set to zero) */
+    freeaml::Vector<double> x = {0.0, 0.0, 0.0, 0.0, 0.0};
+
+    /* right-hand side */
+    freeaml::Vector<double> b = {30.98, 13.03, 9.02, 12.69, 24.13};
+
+    /* linear system solver: Gaussian elimination with partial pivoting */
+    freeaml::GaussianElimination<double> lss(true);
+
+    /* solve the linear system */
+    bool status = lss.solve(A, x, b);
+
+    EXPECT_TRUE(status);
+
+    /* exact solution */
+    freeaml::Vector<double> y = {1.0, 2.0, -1.0, 3.0, 4.0};
+
+    EXPECT_LE((x - y).l2_norm(), 1.e-10);
+}
