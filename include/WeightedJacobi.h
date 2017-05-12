066c68e9e4fe52b48a03afef4740ffab3da21c1f
Diego Assencio <diego@assencio.com>
2017-05-12 19:42:15 +0200

    updates weighted-Jacobi solver for linear systems

---
+++ include/WeightedJacobi.h
@@ -0,0 +1,148 @@
+#pragma once
+
+#include <IterativeLinearSystemSolverBase.h>
+
+namespace freeaml
+{
+template<typename T>
+class WeightedJacobi : public IterativeLinearSystemSolverBase<T>
+{
+public:
+    using BaseSolver = IterativeLinearSystemSolverBase<T>;
+    using size_type = typename BaseSolver::size_type;
+
+    /**
+     * @brief Constructs a linear system solver which uses the weighted-Jacobi
+     *        method.
+     * @param max_iterations The maximum number of iterations allowed.
+     * @param residual_tolerance The residual tolerance.
+     * @param omega The iteration parameter (weight).
+     */
+    WeightedJacobi(size_type max_iterations,
+                   const T& residual_tolerance,
+                   const T& omega);
+
+    /**
+     * @brief Solves a linear system using the weighted-Jacobi method.
+     * @param A The linear system matrix.
+     * @param x The vector on which the solution will be written.
+     * @param b The right-hand side of the linear system.
+     * @return @c true if the residual tolerance could be achieved within the
+     *         maximum number of iterations allowed, @c false otherwise.
+     */
+    template<typename MatrixType, typename VectorType>
+    bool solve(const MatrixType& A, VectorType& x, const VectorType& b);
+
+    /**
+     * @brief Performs a fixed number of weighted-Jacobi iterations for a
+     *        linear system.
+     * @param A The linear system matrix.
+     * @param x The vector on which the solution will be written.
+     * @param b The right-hand side of the linear system.
+     * @param n The number of iterations that will be performed.
+     * @param omega The iteration parameter (weight).
+     * @note This function simply performs @c n weighted-Jacobi iterations
+     *       without taking the residual tolerance into account.
+     */
+    template<typename MatrixType, typename VectorType>
+    static void iterate(const MatrixType& A,
+                        VectorType& x,
+                        const VectorType& b,
+                        size_type n,
+                        const T& omega);
+
+    /**
+     * @brief Gets the iteration parameter (weight) set for the solver.
+     * @return The iteration parameter (weight) set for the solver.
+     */
+    const T& omega() const;
+
+private:
+    const T omega_;
+
+}; /* WeightedJacobi<T> */
+
+/*******************************************************************************
+ *
+ *    FUNCTION DEFINITIONS
+ *
+ ******************************************************************************/
+
+template<typename T>
+WeightedJacobi<T>::WeightedJacobi(const size_type max_iterations,
+                                  const T& residual_tolerance,
+                                  const T& omega)
+    : BaseSolver(max_iterations, residual_tolerance), omega_(omega)
+{
+    FREEAML_ASSERT(omega > T{0});
+}
+
+template<typename T>
+template<typename MatrixType, typename VectorType>
+bool WeightedJacobi<T>::solve(const MatrixType& A,
+                              VectorType& x,
+                              const VectorType& b)
+{
+    FREEAML_ASSERT(BaseSolver::check_dimensions(A, x, b) == true);
+
+    /* r = residual vector */
+    VectorType r = b - A * x;
+
+    (*this).num_iterations_ = 0;
+
+    while ((*this).num_iterations_ < (*this).max_iterations())
+    {
+        for (size_type i = 0; i < x.size(); ++i)
+        {
+            FREEAML_ASSERT(A(i, i) != T{0});
+
+            x[i] += omega() * r[i] / A(i, i);
+        }
+
+        r = b - A * x;
+
+        ++(*this).num_iterations_;
+
+        /* if the residual is within the specified tolerance, stop */
+        if (r.l2_norm() <= (*this).residual_tolerance())
+        {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+template<typename T>
+template<typename MatrixType, typename VectorType>
+void WeightedJacobi<T>::iterate(const MatrixType& A,
+                                VectorType& x,
+                                const VectorType& b,
+                                const size_type n,
+                                const T& omega)
+{
+    FREEAML_ASSERT(BaseSolver::check_dimensions(A, x, b) == true);
+
+    /* r = residual vector */
+    VectorType r = b - A * x;
+
+    for (size_type num_iterations = 0; num_iterations < n; ++num_iterations)
+    {
+        for (size_type i = 0; i < x.size(); ++i)
+        {
+            FREEAML_ASSERT(A(i, i) != T{0});
+
+            x[i] += omega * r[i] / A(i, i);
+        }
+
+        r = b - A * x;
+    }
+}
+
+template<typename T>
+const T& WeightedJacobi<T>::omega() const
+{
+    return omega_;
+}
+
+} /* namespace freeaml */
---
+++ tests/WeightedJacobi.cpp
@@ -0,0 +1,104 @@
+#include <Matrix.h>
+#include <Vector.h>
+#include <WeightedJacobi.h>
+#include <gtest/gtest.h>
+
+TEST(WeightedJacobiTest, Solve4x4LinearSystem)
+{
+    /* linear system matrix */
+    freeaml::Matrix<double> A = {{10.0, -1.0, 2.0, 0.0},
+                                 {-1.0, 11.0, -1.0, 3.0},
+                                 {2.0, -1.0, 10.0, -1.0},
+                                 {0.0, 3.0, -1.0, 8.0}};
+
+    /* solution vector (initially set to zero) */
+    freeaml::Vector<double> x = {0.0, 0.0, 0.0, 0.0};
+
+    /* right-hand side */
+    freeaml::Vector<double> b = {6.0, 25.0, -11.0, 15.0};
+
+    /*
+     * linear system solver: weighted Jacobi
+     * maximum number of iterations: 50
+     * residual tolerance: 1e-7
+     * iteration parameter (weight): 0.5
+     */
+    freeaml::WeightedJacobi<double> lss(50, 1e-7, 0.5);
+
+    /* solve the linear system */
+    bool status = lss.solve(A, x, b);
+
+    EXPECT_TRUE(status);
+
+    double residual = (A * x - b).l2_norm();
+
+    EXPECT_LE(residual, lss.residual_tolerance());
+    EXPECT_LE(lss.num_iterations(), lss.max_iterations());
+
+    /* exact solution */
+    freeaml::Vector<double> y = {1.0, 2.0, -1.0, 1.0};
+
+    EXPECT_LE((x - y).l2_norm(), 1.e-7);
+}
+
+TEST(WeightedJacobiTest, Iterate4x4LinearSystem)
+{
+    /* linear system matrix */
+    freeaml::Matrix<double> A = {{10.0, -1.0, 2.0, 0.0},
+                                 {-1.0, 11.0, -1.0, 3.0},
+                                 {2.0, -1.0, 10.0, -1.0},
+                                 {0.0, 3.0, -1.0, 8.0}};
+
+    /* solution vector (initially set to zero) */
+    freeaml::Vector<double> x = {0.0, 0.0, 0.0, 0.0};
+
+    /* right-hand side */
+    freeaml::Vector<double> b = {6.0, 25.0, -11.0, 15.0};
+
+    /* perform 30 weighted-Jacobi iterations on the linear system */
+    freeaml::WeightedJacobi<double>::iterate(A, x, b, 30, 0.66);
+
+    /* exact solution */
+    freeaml::Vector<double> y = {1.0, 2.0, -1.0, 1.0};
+
+    EXPECT_LE((x - y).l2_norm(), 1.e-7);
+}
+
+TEST(WeightedJacobiTest, Solve5x5LinearSystem)
+{
+    /* linear system matrix */
+    freeaml::Matrix<double> A = {{5.85, 1.05, 1.80, 0.78, 1.16},
+                                 {1.05, 6.52, 0.57, 1.09, 0.34},
+                                 {1.80, 0.57, 7.47, 1.01, 1.31},
+                                 {0.78, 1.09, 1.01, 4.07, 0.15},
+                                 {1.16, 0.34, 1.31, 0.15, 4.37}};
+
+    /* solution vector (initially set to zero) */
+    freeaml::Vector<double> x = {0.0, 0.0, 0.0, 0.0, 0.0};
+
+    /* right-hand side */
+    freeaml::Vector<double> b = {13.13, 18.15, 3.74, 14.76, 18.46};
+
+    /*
+     * linear system solver: weighted Jacobi
+     * maximum number of iterations: 100
+     * residual tolerance: 1e-8
+     * iteration parameter (weight): 0.4
+     */
+    freeaml::WeightedJacobi<double> lss(100, 1e-8, 0.4);
+
+    /* solve the linear system */
+    bool status = lss.solve(A, x, b);
+
+    EXPECT_TRUE(status);
+
+    double residual = (A * x - b).l2_norm();
+
+    EXPECT_LE(residual, lss.residual_tolerance());
+    EXPECT_LE(lss.num_iterations(), lss.max_iterations());
+
+    /* exact solution */
+    freeaml::Vector<double> y = {1.0, 2.0, -1.0, 3.0, 4.0};
+
+    EXPECT_LE((x - y).l2_norm(), 1.e-7);
+}
