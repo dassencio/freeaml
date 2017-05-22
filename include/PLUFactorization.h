f2ee4e50731e113d1a76bf5b47eb33fce9aebbdc
Diego Assencio <diego@assencio.com>
2017-05-22 18:44:21 +0200

    updates PLU factorization class (merged plu and plu_pp into one)

---
+++ include/PLUFactorization.h
@@ -0,0 +1,184 @@
+#pragma once
+
+#include <Vector.h>
+
+namespace freeaml
+{
+class PLUFactorization
+{
+public:
+    /**
+     * @brief Constructs a PLU factorizer object.
+     * @param enable_partial_pivoting @c true if partial pivoting should be
+     *        used, @c false otherwise.
+     */
+    PLUFactorization(bool enable_partial_pivoting = true);
+
+    /**
+     * @brief Computes the PLU factorization of a matrix.
+     * @param A The matrix which will be factorized.
+     * @param P On success, a permutation matrix.
+     * @param L On success, a lower triangular matrix with all diagonal elements
+     *        equal to one (1).
+     * @param U On success, an nonsingular upper triangular matrix.
+     * @return @c true if the factorization <tt>A = PLU</tt> could be built,
+     *         @c false otherwise.
+     * @note The PLU factorization of @c A exists if and only if @c A is
+     *       invertible.
+     */
+    template<typename MatrixType1,
+             typename MatrixType2,
+             typename MatrixType3,
+             typename MatrixType4>
+    bool factorize(MatrixType1 A,
+                   MatrixType2& P,
+                   MatrixType3& L,
+                   MatrixType4& U) const;
+
+    /**
+     * @brief Gets the state set for the partial pivoting technique.
+     * @return @c true if partial pivoting is enabled, @c false otherwise.
+     */
+    bool is_partial_pivoting_enabled() const;
+
+private:
+    const bool enable_partial_pivoting_; /* true: enable partial pivoting */
+
+}; /* PLUFactorization */
+
+/*******************************************************************************
+ *
+ *    FUNCTION DEFINITIONS
+ *
+ ******************************************************************************/
+
+PLUFactorization::PLUFactorization(
+    const bool enable_partial_pivoting /* = true */)
+    : enable_partial_pivoting_(enable_partial_pivoting)
+{
+    /* nothing needs to be done here */
+}
+
+template<typename MatrixType1,
+         typename MatrixType2,
+         typename MatrixType3,
+         typename MatrixType4>
+bool PLUFactorization::factorize(MatrixType1 A,
+                                 MatrixType2& P,
+                                 MatrixType3& L,
+                                 MatrixType4& U) const
+{
+    FREEAML_ASSERT(A.is_square() == true);
+
+    using T = typename MatrixType1::value_type;
+    using size_type = typename MatrixType1::size_type;
+
+    const size_type n = A.num_rows();
+
+    /* assign the correct dimensions to P, L and U */
+    P = MatrixType2(n, n);
+    L = MatrixType3(n, n);
+    U = MatrixType4(n, n);
+
+    /* perm will be used to build the permutation matrix P at the end */
+    Vector<size_type> perm(n);
+
+    /* initialize perm as the "indentity permutation" */
+    for (size_type p = 0; p < n; ++p)
+    {
+        perm[p] = p;
+    }
+
+    /* for each row i of A (except for the last one) */
+    for (size_type i = 0; i + 1 < n; ++i)
+    {
+        size_type p = i;
+
+        /*
+         * if partial pivoting is enabled:
+         *
+         *      find p such that |A(p,i)| = max_q|A(perm[q],i)| for i <= q < n
+         *
+         * if partial pivoting is disabled:
+         *
+         *      find the first p such that A(perm[p],i) != 0 for i <= p < n
+         */
+        if (is_partial_pivoting_enabled() == true)
+        {
+            for (size_type q = i + 1; q < n; ++q)
+            {
+                if (std::abs(A(perm[p], i)) < std::abs(A(perm[q], i)))
+                {
+                    p = q;
+                }
+            }
+
+            /* if A_(perm[p],i) = 0, A is not invertible */
+            if (A(perm[p], i) == T{0})
+            {
+                return false;
+            }
+        }
+        else
+        {
+            while (p < n && A(perm[p], i) == T{0})
+            {
+                ++p;
+            }
+
+            /* if p reaches n, A is not invertible */
+            if (p == n)
+            {
+                return false;
+            }
+        }
+
+        /* "swap" rows i and p of A */
+        std::swap(perm[p], perm[i]);
+
+        for (size_type j = i + 1; j < n; ++j)
+        {
+            A(perm[j], i) /= A(perm[i], i);
+
+            for (size_type l = i + 1; l < n; ++l)
+            {
+                A(perm[j], l) -= A(perm[j], i) * A(perm[i], l);
+            }
+        }
+    }
+
+    /* build P */
+    for (size_type i = 0; i < n; ++i)
+    {
+        P(perm[i], i) = T{1};
+    }
+
+    /* build L */
+    for (size_type i = 0; i < n; ++i)
+    {
+        L(i, i) = T{1};
+
+        for (size_type j = 0; j < i; ++j)
+        {
+            L(i, j) = A(perm[i], j);
+        }
+    }
+
+    /* build U */
+    for (size_type i = 0; i < n; ++i)
+    {
+        for (size_type j = i; j < n; ++j)
+        {
+            U(i, j) = A(perm[i], j);
+        }
+    }
+
+    return true;
+}
+
+bool PLUFactorization::is_partial_pivoting_enabled() const
+{
+    return enable_partial_pivoting_;
+}
+
+} /* namespace freeaml */
---
+++ tests/PLUFactorization.cpp
@@ -0,0 +1,68 @@
+#include <Matrix.h>
+#include <PLUFactorization.h>
+#include <SparseMatrix.h>
+#include <Vector.h>
+#include <gtest/gtest.h>
+
+TEST(PLUFactorizationTest, Factorize4x4MatrixWithoutPartialPivoting)
+{
+    /* matrix to be factorized */
+    freeaml::Matrix<double> A = {{0.00, 4.20, 4.25, 1.73},
+                                 {0.00, 1.23, 3.89, 2.11},
+                                 {4.63, 0.00, 1.51, 2.67},
+                                 {3.70, 0.00, 0.31, 3.84}};
+
+    freeaml::SparseMatrix<double> P;
+    freeaml::Matrix<double> L, U;
+
+    /* PLU factorizer without partial pivoting */
+    freeaml::PLUFactorization fac(false);
+
+    /* compute the PLU factorization A = PLU */
+    bool status = fac.factorize(A, P, L, U);
+
+    EXPECT_TRUE(status);
+
+    /* compute P^t*P */
+    freeaml::SparseMatrix<double> PtP = P.transpose() * P;
+
+    /* I is the 4×4 identity matrix */
+    freeaml::SparseMatrix<double> I =
+        freeaml::identity_sparse_matrix<double>(4);
+
+    EXPECT_LE((A - P * L * U).max_norm(), 1.e-10);
+    EXPECT_LE((PtP - I).max_norm(), 1.e-10);
+    EXPECT_NE(P, I);
+}
+
+TEST(PLUFactorizationTest, Factorize5x5MatrixWithPartialPivoting)
+{
+    /* matrix to be factorized */
+    freeaml::Matrix<double> A = {{0.00, 1.57, 2.68, 4.38, 3.65},
+                                 {2.56, 1.94, 1.98, 1.75, 0.83},
+                                 {1.80, 1.25, 2.38, 0.62, 1.31},
+                                 {2.59, 2.13, 0.65, 1.67, 0.37},
+                                 {1.48, 2.52, 2.34, 0.53, 4.59}};
+
+    freeaml::SparseMatrix<double> P;
+    freeaml::Matrix<double> L, U;
+
+    /* PLU factorizer with partial pivoting */
+    freeaml::PLUFactorization fac(true);
+
+    /* compute the PLU factorization A = PLU */
+    bool status = fac.factorize(A, P, L, U);
+
+    EXPECT_TRUE(status);
+
+    /* compute P^t*P */
+    freeaml::SparseMatrix<double> PtP = P.transpose() * P;
+
+    /* I is the 5×5 identity matrix */
+    freeaml::SparseMatrix<double> I =
+        freeaml::identity_sparse_matrix<double>(5);
+
+    EXPECT_LE((A - P * L * U).max_norm(), 1.e-10);
+    EXPECT_LE((PtP - I).max_norm(), 1.e-10);
+    EXPECT_NE(P, I);
+}
